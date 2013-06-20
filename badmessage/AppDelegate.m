04ba6b19d4036c718dccea08501f3cd1dd58af8e
Eric Gorr <gorr_eric@bah.com>
2013-06-20 15:42:13 -0400

    initial commit

--- badmessage/AppDelegate.m
+++ badmessage/AppDelegate.m
@@ -7,12 +7,32 @@
 //
 
 #import "AppDelegate.h"
+#import <MailCore/MailCore.h>
+
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    NSBundle*   mainBundle  = [NSBundle mainBundle];
+    NSString*   messagePath = [mainBundle pathForResource:@"29193" ofType:@"emlx"];
+    
+    NSLog( @"%@", messagePath );
+    
+    NSData*             emlxMessageData = [[NSData alloc] initWithContentsOfFile:messagePath];
+    NSRange             dataRange       = NSMakeRange( 11, 108286 );
+    NSData*             messageData     = [emlxMessageData subdataWithRange:dataRange];
+    MCOMessageParser*   parser          = [MCOMessageParser messageParserWithData:messageData];
+    MCOAbstractPart*    mainPart        = [parser mainPart];
+    MCOAbstractMessage* message         = [mainPart message];
+    MCOMessageHeader*   header          = [message header];
+    
+    NSLog( @"%@", [header subject] );
+    NSLog( @"%u", [mainPart partType] );
+    
+    NSArray*    parts = [(MCOAbstractMultipart*)mainPart parts];
+    
+    NSLog( @"%@", parts );
 }
 
 @end
